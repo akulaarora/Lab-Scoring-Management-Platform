c53e53ecf0ec33cf3c5a669654a9ba81d41e01be
Max Nova <37260107+MaxNova-CompSci@users.noreply.github.com>
2018-06-01 00:40:42 -0700

    Contains all method sigs. Just need to implement functional characteristics

--- Lab-Scoring-Management-Platform/src/scoringmanagement/Scorer.java
+++ Lab-Scoring-Management-Platform/src/scoringmanagement/Scorer.java
@@ -2,11 +2,16 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import java.net.URL;
 import java.net.URLClassLoader;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import javax.tools.Diagnostic;
 import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
@@ -18,7 +23,7 @@ import javax.tools.ToolProvider;
  * Main scoring class for student lab submissions. Receives the student's lab files from the "LabSubmission" class
  * and compares them scoring various sections of the program based on the spec for said lab.
  *
- * @author Max, Darshan, Akul, Manseej
+ * @author Max, Darshan
  * @version 5/29
  */
 public class Scorer
@@ -40,28 +45,61 @@ public class Scorer
      * Acts as the main method and is the only outward facing method of class.
      * scoreStudent calls all other methods within the class.
      * Compilation of the student's labs is done by the Java provided library --> https://docs.oracle.com/javase/7/docs/api/javax/tools/JavaCompiler.html
-     *
+     * Compilation section is forked from ---> https://github.com/0416354917/Algorithms/blob/master/src/util/InlineCompiler.java
+     * Bash setup and commandline is forked from ---> https://stackoverflow.com/questions/26830617/java-running-bash-commands
+     * Bash exectution is run by --> https://docs.oracle.com/javase/7/docs/api/java/lang/ProcessBuilder.html
+     * 
      * @param Sends array of labFiles
      * @return scoreData array holds the score output of the student's lab and returns the output to their student client
      */
     public static void scoreStudent(String[] labFiles)
     {
-        String[] scoreData = new String[5];//scoreData is returned to the 
+        //Read the lab spec and set the number of inputs required to the size of the object
+        //int inputNum = ???;
+        
+        Object[][] scoreData;
+        Object[] labData;
        
+        
+        
+        /** BASH EXECUTION SECTION */
+        
+        //calls private methods and returns calculated string as output
+        //Score does not store data in the database
+        //Student submission calls score which returns the String[] of information and then Student submission call the dbinteract and enters all the information
+        //Score newScore = new Score(scoreData, labData);//only create the object once the variables have been created and set.
+    }
+    
+    /**
+     * The compileLab method takes in lab submission data (ID, LabName, and LabFiles)
+     * A directory is setup such as below that sandboxes each run within the user's unique directory.
+     * Value returned determines if the program was able to compile the file(s) or not
+     * 
+     * Sample Directory:
+     * 1849999---> (newFolder) Lab30 --> labFiles (.class added after compilation)
+     *        ^--> (new Folder) lab31 --> etc
+     *        
+     * @param String labName
+     * @param String ID
+     * @param String[] labFiles
+     */
+    public boolean compileLab(String labName, String ID, String[] labFiles)
+    {
+        boolean testCompile = false;
         StringBuilder sb = new StringBuilder(64);//WHAT DOES 64 EVEN DO?
         //IMPLEMENT FOR LOOP TO APPEND EACH LINE AT EVERY NEWLINE
         //FOR SOME REASON JUST APPENDING THE ENTIRE LAB THROUGH A SINGLE METHOD CALL LIKE A MAD MAN IS A BAD IDEA. BREAKS I DUNNO WHY, DOCUMENTATION IS A PAIN IN THE ASS TO READ. COMMENTED OUT CODE WORKS THOUGH  
-        /*
+        
         sb.append("public class HelloWorld {\n");
         sb.append("    public static void main(String[] args) {\n");
         sb.append("        System.out.println(\"Hello world!\");\n");
         sb.append("    }\n");
         sb.append("}\n");    
-        */
-        sb.append(labFiles[0]);
+        
+        //sb.append(labFiles[0]);
        
         //CHANGE TO SERVER DIRECTORY 
-        File helloWorldJava = new File("C:\\Users\\^Water_Bear\\Desktop\\compiletest\\HelloWorld.java");
+        File helloWorldJava = new File("C:/Users/^Water_Bear/Desktop/compiletest/HelloWorld.java");
         if (helloWorldJava.getParentFile().exists() || helloWorldJava.getParentFile().mkdirs())
         {
             try
@@ -73,7 +111,7 @@ public class Scorer
                     writer.write(sb.toString());
                     writer.flush();
                 } 
-                finally //what even is a finally!??!
+                finally 
                 {
                     try 
                     {
@@ -134,27 +172,133 @@ public class Scorer
                 exp.printStackTrace();
             }
         }
-        //calls private methods and returns calculated string as output
-        //Score does not store data in the database
-        //Student submission calls score which returns the String[] of information and then Student submission call the dbinteract and enters all the information
-        Score newScore = new Score(scoreData);
+        
+        return testCompile;
     }
     
-    /*
-     * List of methods to implement--All strings outputted are stored in scoreData
-     * private static String labOutput(compiled labFile)
-     * --gets spec output data and compares it with the compiled file
-     * --get the program to work dynamically with requiring user input
-     * private static String labComments(source labFile)
-     * --Outputs percentage of comments to code written maybe?
-     * --Check to make sure java docs exist for each method and the class
-     * --counts the number of inline comments
-     * private static String checkMethods(source labFile)
-     * --Make sure the methods defined in the spec are synchronized with what is within the lab
-     * private static String ackChecker(source labFile)
-     * --check for break statements and count the number of returns for each method
-     * private static String 
+    /**
+     * The execLab method takes in lab submission data (ID, LabName, and LabFiles)
+     * A directory is setup such as below such that once execLab is run after compilation it creates a new folder holding the 
+     * lab's output in a txt file
      * 
+     * Sample Directory:
+     * 1849999---> (newFolder) Lab30 --> labFiles (.class added after compilation)
+     *        ^                      --> (newFolder) outputFile1
+     *        ^--> (new Folder) lab31 --> etc
+     *        
+     * @param String ID
+     * @param String labName
+     * @param String mainFile
      * 
+     * @return Object[] scoreNrunTest
      */
+    public Object[][] execLab(String ID, String labName, String mainFile)
+    {
+        //--gets spec output data and compares it with the compiled file
+        //--get the program to work dynamically with requiring user input
+        final int arrayWidth = 5;
+        Boolean runTest = new Boolean("false");
+        //get lab spec which is formatted like
+        /*
+         * #number of inputs 3
+         * //Specific set of inputs
+         */
+        Object[][] scoreNrunTest = new Object[3][arrayWidth];
+        for(int i = 0; i < 3; i++)
+        {
+            scoreNrunTest[i][arrayWidth] = i;
+        }
+        /*
+         * ------------------------------------------  
+         * | 0    | 20.0 | 65%    | True    | False |       
+         * | 1    | 30.0 | 80%    | False   | True  |       METHOD DEFINES ALL THESE VALUES
+         * | 2    | 10.0 | 10%    | True    | False |       
+         * ------------------------------------------
+         */
+        
+        /*
+          * cd into correct directory
+          * Create output folder
+          * try to run main file and catch execp
+          * read output in bash and cat to file in directory
+          * return if the program ran
+          */
+         
+        return scoreNrunTest;
+    }
+    
+    /**
+     * Allows commands to executed through bash.
+     * Once a program has been compiled and set into its appropriate directory the files can be interacted with this method.
+     * Adapated from --> https://stackoverflow.com/questions/26830617/java-running-bash-commands
+     * 
+     * @param String command
+     * @return String output
+     */
+    public String executeCommand(String command) 
+    {
+        StringBuffer output = new StringBuffer();
+        Process p;
+        try 
+        {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader = 
+                        new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";           
+            while ((line = reader.readLine())!= null) 
+            {
+                output.append(line + "\n");
+            }
+
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+
+    }
+    
+    public int numComments(String ID, String labName)
+    {
+       int commentNum = 0;
+        /*
+        --Outputs percentage of comments to code written maybe?
+       --Check to make sure java docs exist for each method and the class
+       --counts the number of inline comments
+       */
+       return commentNum;
+    }
+    
+    public int numMethods(String ID, String labName)
+    {
+        int methodNum = 0;
+        //Check to see how many methods are defined 
+        return methodNum;
+    }
+    
+    public int ackCounter(String ID, String labName)
+    {
+        int breakNum = 0;
+        //check for break statements and count the total
+        return breakNum;
+    }
+    
+    public int numJavaDoc(String ID, String labName)
+    {
+        int javaDocNum = 0;
+        //check for the number of java docs in the program 
+        //could be used in conjunction with the numMethods method and compared + private methods screw things up but that does not really matter
+        return javaDocNum;
+    }
+    
+    public boolean globalInstanceCheck(String ID, String labName)
+    {
+        boolean instanceCheck = false;
+        //This method checks to see if global variables exists "ACK" bad mojo //emi rigt
+        return instanceCheck;
+    }
 }
