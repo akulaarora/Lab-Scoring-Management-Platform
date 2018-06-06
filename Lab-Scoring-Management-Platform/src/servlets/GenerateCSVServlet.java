1dbcc6c121393d67af11114a92443a57c4a37249
akulaarora <akularora@gmail.com>
2018-06-05 22:51:49 -0700

    Quick edits to fix servlet filepath

--- Lab-Scoring-Management-Platform/src/servlets/GenerateCSVServlet.java
+++ Lab-Scoring-Management-Platform/src/servlets/GenerateCSVServlet.java
@@ -28,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class GenerateCSVServlet
  */
-@WebServlet("/GenerateCSVServlet")
+@WebServlet("/Teacher/GenerateCSVServlet")
 public class GenerateCSVServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
