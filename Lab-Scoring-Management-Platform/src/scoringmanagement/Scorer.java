aae095577fa5640b4de44a4977acf1cfab25ff6a
Max Nova <37260107+MaxNova-CompSci@users.noreply.github.com>
2018-06-06 19:12:23 -0700

    getScore works r333

--- Lab-Scoring-Management-Platform/src/scoringmanagement/Scorer.java
+++ Lab-Scoring-Management-Platform/src/scoringmanagement/Scorer.java
@@ -2,11 +2,16 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import java.net.URL;
 import java.net.URLClassLoader;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import javax.tools.Diagnostic;
 import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
@@ -14,11 +19,15 @@ import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
 import javax.tools.ToolProvider;
 
+import java.util.*;
+import chn.util.*;
+import java.util.Scanner.*;
+
 /**
  * Main scoring class for student lab submissions. Receives the student's lab files from the "LabSubmission" class
  * and compares them scoring various sections of the program based on the spec for said lab.
  *
- * @author Max, Darshan, Akul, Manseej
+ * @author Max, Darshan
  * @version 5/29
  */
 public class Scorer
@@ -40,121 +49,144 @@ public class Scorer
      * Acts as the main method and is the only outward facing method of class.
      * scoreStudent calls all other methods within the class.
      * Compilation of the student's labs is done by the Java provided library --> https://docs.oracle.com/javase/7/docs/api/javax/tools/JavaCompiler.html
-     *
+     * Compilation section is forked from ---> https://github.com/0416354917/Algorithms/blob/master/src/util/InlineCompiler.java
+     * Bash setup and commandline is forked from ---> https://stackoverflow.com/questions/26830617/java-running-bash-commands
+     * Bash exectution is run by --> https://docs.oracle.com/javase/7/docs/api/java/lang/ProcessBuilder.html
+     * 
      * @param Sends array of labFiles
      * @return scoreData array holds the score output of the student's lab and returns the output to their student client
      */
     public static void scoreStudent(String[] labFiles)
     {
-        String[] scoreData = new String[5];//scoreData is returned to the 
+        //Read the lab spec and set the number of inputs required to the size of the object
+        //int inputNum = ???;
+        
+        Object[] scoreData;
+        Object[] labData;
        
-        StringBuilder sb = new StringBuilder(64);//WHAT DOES 64 EVEN DO?
-        //IMPLEMENT FOR LOOP TO APPEND EACH LINE AT EVERY NEWLINE
-        //FOR SOME REASON JUST APPENDING THE ENTIRE LAB THROUGH A SINGLE METHOD CALL LIKE A MAD MAN IS A BAD IDEA. BREAKS I DUNNO WHY, DOCUMENTATION IS A PAIN IN THE ASS TO READ. COMMENTED OUT CODE WORKS THOUGH  
-        /*
-        sb.append("public class HelloWorld {\n");
-        sb.append("    public static void main(String[] args) {\n");
-        sb.append("        System.out.println(\"Hello world!\");\n");
-        sb.append("    }\n");
-        sb.append("}\n");    
-        */
-        sb.append(labFiles[0]);
-       
-        //CHANGE TO SERVER DIRECTORY 
-        File helloWorldJava = new File("C:\\Users\\^Water_Bear\\Desktop\\compiletest\\HelloWorld.java");
-        if (helloWorldJava.getParentFile().exists() || helloWorldJava.getParentFile().mkdirs())
-        {
-            try
-            {
-                Writer writer = null;
-                try
-                {
-                    writer = new FileWriter(helloWorldJava);
-                    writer.write(sb.toString());
-                    writer.flush();
-                } 
-                finally //what even is a finally!??!
-                {
-                    try 
-                    {
-                        writer.close();
-                    } 
-                    catch (Exception e) 
-                    {
-                        System.out.println("writer can't be closed");
-                    }
-                }
-
-                /** Compilation Requirements *********************************************************************************************/
-                DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
-                JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-                StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null);
-
-                // This sets up the class path that the compiler will use
-                List<String> optionList = new ArrayList<String>();
-                optionList.add("-classpath");
-                optionList.add(System.getProperty("java.class.path") + ";dist/InlineCompiler1.jar");
-
-                Iterable<? extends JavaFileObject> compilationUnit
-                        = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(helloWorldJava));
-                JavaCompiler.CompilationTask task = compiler.getTask(
-                    null, 
-                    fileManager, 
-                    diagnostics, 
-                    optionList, 
-                    null, 
-                    compilationUnit);
-                /********************************************************************************************* Compilation Requirements **/
-                if (task.call()) 
-                {
-                    /** Load and execute *************************************************************************************************/
-                    // Create a new custom class loader, pointing to the directory that contains the compiled
-                    // classes, this should point to the top of the package structure!
-                    @SuppressWarnings("resource")
-                    URLClassLoader classLoader = new URLClassLoader(new URL[]{new File("./").toURI().toURL()});
-                    // Load the class from the classloader by name....
-                    Class<?> loadedClass = classLoader.loadClass("testcompile.HelloWorld");
-                    // Create a new instance...
-                    Object obj = loadedClass.newInstance();
-                    /************************************************************************************************* Load and execute **/
-                } 
-                else 
-                {
-                    for (Diagnostic<? extends JavaFileObject> diagnostic : diagnostics.getDiagnostics()) 
-                    {
-                        System.out.format("Error on line %d in %s%n",
-                            diagnostic.getLineNumber(),
-                            diagnostic.getSource().toUri());
-                    }
-                }
-                fileManager.close();
-            } 
-            catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException exp) 
-            {
-                exp.printStackTrace();
-            }
-        }
+        System.out.println(getScore("lab30_1", "1849935", true, false));
+        
+        /** BASH EXECUTION SECTION */
+        
         //calls private methods and returns calculated string as output
         //Score does not store data in the database
         //Student submission calls score which returns the String[] of information and then Student submission call the dbinteract and enters all the information
-        Score newScore = new Score(scoreData);
+        //Score newScore = new Score(scoreData, labData);//only create the object once the variables have been created and set.
     }
     
-    /*
-     * List of methods to implement--All strings outputted are stored in scoreData
-     * private static String labOutput(compiled labFile)
-     * --gets spec output data and compares it with the compiled file
-     * --get the program to work dynamically with requiring user input
-     * private static String labComments(source labFile)
-     * --Outputs percentage of comments to code written maybe?
-     * --Check to make sure java docs exist for each method and the class
-     * --counts the number of inline comments
-     * private static String checkMethods(source labFile)
-     * --Make sure the methods defined in the spec are synchronized with what is within the lab
-     * private static String ackChecker(source labFile)
-     * --check for break statements and count the number of returns for each method
-     * private static String 
-     * 
-     * 
-     */
+    public static double getScore(String labName, String ID, boolean testExec, boolean testComp)
+    {
+        FileInput correctFile = new FileInput("C:/Users/^Water_Bear/Desktop/1849935/lab30_1/output.txt");;
+        FileInput wrongFile;
+        FileInput countFile;
+        
+        double avgScore = 0.0;
+        int numLines = 0;
+        int totalOutputScore = 30;
+        
+        if(testExec == true)
+        {
+            avgScore+=10;
+        }
+        if(testComp == true)
+        {
+            avgScore+=10;
+        }
+        
+        
+        Boolean[] checkOut;
+        String[] labSpec = {"cow went moo", "chick goes bock", "107"};;
+        
+        try
+        {
+            countFile = new FileInput("C:/Users/^Water_Bear/Desktop/1849935/lab30_1/output.txt");
+
+            while(countFile.hasMoreLines())
+            {    
+                numLines++;
+                countFile.readLine();
+            }   
+            
+            countFile.close();
+            
+            
+        }
+        catch(Exception e)
+        {
+            System.out.println("Make sure to first compile and execute the program before calling this method!!");
+        }
+        
+        String[] labFile = new String[numLines];
+        checkOut = new Boolean[numLines];
+        int x = 0;
+        while(correctFile.hasMoreLines())
+        {
+            labFile[x] = correctFile.readLine().toLowerCase(); 
+            x++;
+        }
+        
+        for(int i = 0; i < labFile.length; i++)
+        {
+            for(int j = 0; j < labSpec.length; j++)
+            {
+                if(labSpec[j].equals(labFile[i]))
+                {
+                    checkOut[i] = true;
+                }
+            }
+            if(checkOut[i] == null)
+                checkOut[i] = false;
+        }
+        
+        int checksCorrect = 0;
+        for(int k = 0; k < checkOut.length; k++)
+        {
+            if(checkOut[k] == true)
+                checksCorrect++;   
+        }
+        
+        avgScore += ((((double)checksCorrect)/(checkOut.length))*totalOutputScore);
+
+        return avgScore;
+    }
+    
+    public int numComments(String ID, String labName)
+    {
+       int commentNum = 0;
+        /*
+        --Outputs percentage of comments to code written maybe?
+       --Check to make sure java docs exist for each method and the class
+       --counts the number of inline comments
+       */
+       return commentNum;
+    }
+    
+    public int numMethods(String ID, String labName)
+    {
+        int methodNum = 0;
+        //Check to see how many methods are defined 
+        return methodNum;
+    }
+    
+    public int ackCounter(String ID, String labName)
+    {
+        int breakNum = 0;
+        //check for break statements and count the total
+        return breakNum;
+    }
+    
+    public int numJavaDoc(String ID, String labName)
+    {
+        int javaDocNum = 0;
+        //check for the number of java docs in the program 
+        //could be used in conjunction with the numMethods method and compared + private methods screw things up but that does not really matter
+        return javaDocNum;
+    }
+    
+    public boolean globalInstanceCheck(String ID, String labName)
+    {
+        boolean instanceCheck = false;
+        //This method checks to see if global variables exists "ACK" bad mojo //emi rigt
+        return instanceCheck;
+    }
 }
