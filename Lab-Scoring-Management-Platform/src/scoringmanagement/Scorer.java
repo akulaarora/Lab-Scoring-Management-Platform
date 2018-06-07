72d367b1c7aecbac9d7b52ce32d823549a8643ec
Max Nova <37260107+MaxNova-CompSci@users.noreply.github.com>
2018-06-07 00:24:02 -0700

    COMPLETE

--- Lab-Scoring-Management-Platform/src/scoringmanagement/Scorer.java
+++ Lab-Scoring-Management-Platform/src/scoringmanagement/Scorer.java
@@ -1,5 +1,3 @@
-package scoringmanagement;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,6 +23,14 @@ import java.util.*;
 import chn.util.*;
 import java.util.Scanner.*;
 
+import chn.util.*;
+import java.util.Scanner.*;
+import java.io.File;
+import java.lang.Object;
+import java.io.*;
+import org.apache.commons.lang3.StringUtils;//https://commons.apache.org/proper/commons-lang/download_lang.cgi
+import org.apache.commons.io.FileUtils;//http://commons.apache.org/proper/commons-io/download_io.cgi
+
 /**
  * Main scoring class for student lab submissions. Receives the student's lab files from the "LabSubmission" class
  * and compares them scoring various sections of the program based on the spec for said lab.
@@ -34,19 +40,6 @@ import java.util.Scanner.*;
  */
 public class Scorer
 {
-    public static void main(String[] args)
-    {
-        String testLab = "package testcompile; public class HelloWorld implements inlinecompiler.InlineCompiler.DoStuff{ public static void main(String[] args) {System.out.println(\"Hello world\");}}";
-        String[] labFiles = new String[1];
-        labFiles[0] = testLab;
-        scoreStudent(labFiles);
-        
-        //https://stackoverflow.com/questions/21544446/how-do-you-dynamically-compile-and-load-external-java-classes?noredirect=1&lq=1
-        
-        //WE NEED TO REMOVE ALL NEWLINES FROM THE STRING + ADD ESCAPE CHARACTERS FOR ALL " IN THE CODE -- create parser
-        //maybe keep the newlines? I think the program breaks without them
-    }
-    
     /**
      * Acts as the main method and is the only outward facing method of class.
      * scoreStudent calls all other methods within the class.
@@ -55,30 +48,60 @@ public class Scorer
      * Bash setup and commandline is forked from ---> https://stackoverflow.com/questions/26830617/java-running-bash-commands
      * Bash exectution is run by --> https://docs.oracle.com/javase/7/docs/api/java/lang/ProcessBuilder.html
      * 
+     * @param String labSpec
      * @param Sends array of labFiles
      * @return scoreData array holds the score output of the student's lab and returns the output to their student client
      */
-    public static void scoreStudent(String[] labFiles)
+    public static void scoreStudent(String labSpec, String[] labFiles, String[] labNames)
     {
-        //Read the lab spec and set the number of inputs required to the size of the object
-        //int inputNum = ???;
-        
         Object[] scoreData;
         Object[] labData;
-       
-        System.out.println(getScore("lab30_1", "1849935", true, false));
+        Boolean testCompile = false;
+        Boolean testExec;
+        Double score;
+        Integer numCom, numAck, numDoc, numRet;
         
-        /** BASH EXECUTION SECTION */
         
-        //calls private methods and returns calculated string as output
-        //Score does not store data in the database
-        //Student submission calls score which returns the String[] of information and then Student submission call the dbinteract and enters all the information
-        //Score newScore = new Score(scoreData, labData);//only create the object once the variables have been created and set.
+        //COMPILE THE LABS
+        for(int i = 0; i < labFiles.length; i++)
+        {
+            Boolean compOutput;
+            compOutput = Invoker.compileLab(labNames[i], labFiles[i]);
+            if(compOutput = true)
+                testCompile = true;
+        }
+        
+        //RUN THE MAIN FILE
+        testExec = Invoker.runProgram("Driver");
+        
+        //Score the lab
+        score = getScore(labSpec, testExec, testCompile);
+        
+        //Create user stats
+        numCom = numComments();
+        numAck = ackCounter();
+        numDoc = numJavaDoc();
+        numRet = numReturn();
+        
+        scoreData = new Object[] {score, testCompile, testExec};
+        labData = new Object[] {numCom, numDoc, numAck, numRet};
+        
+        //Score object instantiated - use getters to access its components
+        Score studentScore = new Score(scoreData, labData);
     }
     
-    public static double getScore(String labName, String ID, boolean testExec, boolean testComp)
+    /**
+     * Allows a user to score their lab based on compilation, execution, and their program output
+     * 
+     * @param String specName
+     * @param boolean testExec
+     * @param boolean testComp
+     * 
+     * @return double avgScore
+     */
+    public static double getScore(String specName, boolean testExec, boolean testComp)
     {
-        FileInput correctFile = new FileInput("C:/Users/^Water_Bear/Desktop/1849935/lab30_1/output.txt");;
+        FileInput correctFile = new FileInput("C:/Users/^Water_Bear/Desktop/temp/output.txt");
         FileInput wrongFile;
         FileInput countFile;
         
@@ -95,9 +118,8 @@ public class Scorer
             avgScore+=10;
         }
         
-        
         Boolean[] checkOut;
-        String[] labSpec = {"cow went moo", "chick goes bock", "107"};;
+        String[] labSpec = {"cow went moo", "chick goes bock", "107"};//getAllOut(String specName)
         
         try
         {
@@ -152,43 +174,187 @@ public class Scorer
         return avgScore;
     }
     
-    public int numComments(String ID, String labName)
+    /**
+     * Scans the user directory and returns all the .java files within it.
+     * These files are then accessed and used to for Scorer
+     * 
+     * https://stackoverflow.com/questions/2102952/listing-files-in-a-directory-matching-a-pattern-in-java
+     * 
+     * @return File[] files
+     */
+    public static File[] scanDirectory()
+    {
+        File dir = new File("C:/Users/^Water_Bear/Desktop/temp");//CHANGE TO SERVER DIRECTORY
+        File[] files = dir.listFiles(new FilenameFilter() 
+        {
+            @Override
+            public boolean accept(File dir, String name) 
+            {
+                return name.endsWith(".java");
+            }
+        });
+        
+        return files;
+    }
+    
+    private String getAfterOut(String line)
+    {
+        String output;        
+        int i = line.indexOf("OUT");
+        
+        output = line.substring(i + 4);
+        return output;
+    } 
+    
+    /**
+     * Finds the specific lab spec and scans it for all output to be cross referenced in Score
+     * 
+     * @param String specName
+     * 
+     * @return output
+     */
+    public String[] getAllOut(String specName)
+    {
+        String specDirectory = "/Users/darshanparekh/Documents/test/" + specName + ".txt";//change to server directory
+        String indLine;
+        ArrayList<String> outList = new ArrayList<String>();
+        
+        FileInput inFile= new FileInput(specDirectory);
+        while(inFile.hasMoreLines())
+        {
+            indLine = inFile.readLine();
+            if(indLine.indexOf("OUT") >= 0)
+            {
+                indLine = getAfterOut(indLine);
+                outList.add(indLine);
+            }
+        }
+        
+        String[] output = new String[outList.size()];
+        for(int i = 0; i < outList.size(); i++)
+        {
+            output[i] = outList.get(i);
+        }
+        
+        return output;
+    }
+    
+    /**
+     * Counts the number of comments in all labFiles the student has submit. NOT APPLIED TO THEIR SCORE!
+     * This data is returned to the student only as a statistic.
+     * 
+     * @return int commentNum
+     */
+    public static int numComments()
     {
        int commentNum = 0;
-        /*
-        --Outputs percentage of comments to code written maybe?
-       --Check to make sure java docs exist for each method and the class
-       --counts the number of inline comments
-       */
+       File[] files;
+       files = scanDirectory();
+       
+       for(int i = 0; i < files.length; i++)
+       {
+           try
+           {
+               String code = FileUtils.readFileToString(files[i]);
+               
+               commentNum += StringUtils.countMatches(code, "//");
+           }
+           catch(Exception e)
+           {
+               System.out.println("Could not convert File object to String object");
+           }
+       }
+       
        return commentNum;
     }
     
-    public int numMethods(String ID, String labName)
-    {
-        int methodNum = 0;
-        //Check to see how many methods are defined 
-        return methodNum;
-    }
-    
-    public int ackCounter(String ID, String labName)
+    /**
+     * Counts the number of break statements in all labFiles the student has submit. NOT APPLIED TO THEIR SCORE!
+     * This data is returned to the student only as a statistic
+     * 
+     * @return int breakNum
+     */
+    public static int ackCounter()
     {
         int breakNum = 0;
-        //check for break statements and count the total
+        
+        File[] files;
+        files = scanDirectory();
+       
+        for(int i = 0; i < files.length; i++)
+        {
+           try
+           {
+               String code = FileUtils.readFileToString(files[i]);
+               
+               breakNum += StringUtils.countMatches(code, "break");
+           }
+           catch(Exception e)
+           {
+               System.out.println("Could not convert File object to String object");
+           }
+        }
+        
         return breakNum;
     }
     
-    public int numJavaDoc(String ID, String labName)
+    /**
+     * Counts the number of javaDocs in all labFiles the student has submit. NOT APPLIED TO THEIR SCORE!
+     * This data is returned to the student only as a statistic
+     * 
+     * @return int javaDocNum
+     */
+    public static int numJavaDoc()
     {
         int javaDocNum = 0;
-        //check for the number of java docs in the program 
-        //could be used in conjunction with the numMethods method and compared + private methods screw things up but that does not really matter
+        
+        File[] files;
+        files = scanDirectory();
+       
+        for(int i = 0; i < files.length; i++)
+        {
+           try
+           {
+               String code = FileUtils.readFileToString(files[i]);
+               
+               javaDocNum += StringUtils.countMatches(code, "/**");
+           }
+           catch(Exception e)
+           {
+               System.out.println("Could not convert File object to String object");
+           }
+        }
+        
         return javaDocNum;
     }
     
-    public boolean globalInstanceCheck(String ID, String labName)
+    /**
+     * Counts the number of returns in all labFiles the student has submit. NOT APPLIED TO THEIR SCORE!
+     * This data is returned to the student only as a statistic
+     * 
+     * @return int instanceCheck
+     */
+    public static int numReturn()
     {
-        boolean instanceCheck = false;
-        //This method checks to see if global variables exists "ACK" bad mojo //emi rigt
+        int instanceCheck = 0;
+        
+        File[] files;
+        files = scanDirectory();
+       
+        for(int i = 0; i < files.length; i++)
+        {
+           try
+           {
+               String code = FileUtils.readFileToString(files[i]);
+               
+               instanceCheck += StringUtils.countMatches(code, "return");
+           }
+           catch(Exception e)
+           {
+               System.out.println("Could not convert File object to String object");
+           }
+        }
+        
         return instanceCheck;
     }
 }
